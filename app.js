f7aa43a8331d856f10eb6ef5c9b95f2dbfbe10dc
jeongdonggi <dkrhank45@gmail.com>
2022-12-21 02:28:12 +0900

    첫번쨰

---
+++ app.js
@@ -0,0 +1,22 @@
+// express 사용
+"use strict"
+
+const express = require('express')
+const app = express()
+var cors = require('cors')
+//host
+const port = 3000
+
+app.use(cors())
+
+//앱세팅
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+const home = require("./routes/home")
+app.use("/",home) // use -> 미들 웨어 등록
+
+//listen: 포트 https: 443
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
